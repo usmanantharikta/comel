3ce51539b0ba48d08af8b489b59fe1a8ff4aae93
usmanantharikta <antharikt7@gmail.com>
2017-03-09 15:46:20 +0700

    dssfs

--- index.php
+++ index.php
@@ -63,16 +63,16 @@ $app->post('/', function ($request, $response)
 
 });
 
-// $app->get('/push/{to}/{message}', function ($request, $response, $args)
-// {
-// 	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN']);
-// 	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV['CHANNEL_SECRET']]);
+$app->get('/push/{to}/{message}', function ($request, $response, $args)
+{
+	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN']);
+	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV['CHANNEL_SECRET']]);
 
-// 	$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($args['message']);
-// 	$result = $bot->pushMessage($args['to'], $textMessageBuilder);
+	$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($args['message']);
+	$result = $bot->pushMessage($args['to'], $textMessageBuilder);
 
-// 	return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-// });
+	return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+});
 
 /* JUST RUN IT */
 $app->run();
